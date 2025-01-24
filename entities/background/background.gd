6efe3f8249a0976dc3ea4259b357038b076b5635
Vinícius Hoyer <contact@vhoyer.dev>
2025-01-24 16:41:43 -0300

    ✨ Fix background sky

--- entities/background/background.gd
+++ entities/background/background.gd
@@ -1,3 +1,4 @@
+@tool
 extends CanvasLayer
 class_name Background
 
@@ -7,10 +8,14 @@ signal update_height(height: float)
 @export var height_max: float = 2000
 @export var height_min: float = 0
 
+@export var debug_height: float = 0:
+	set(value):
+		debug_height = value
+		update_height.emit(value)
+
 func _ready() -> void:
 	update_height.connect(pass_height_to_shader)
 
 func pass_height_to_shader(height: float) -> void:
-	var normal_height = (height - height_max)/(height_min - height_max);
-	print(normal_height)
+	var normal_height = (height_min - height)/(height_max - height_min);
 	texture_rect.material.set_shader_parameter("height", normal_height)
